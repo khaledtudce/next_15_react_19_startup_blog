967686dd7f86359f79166c3ebb443a47b117b567
khaled Reza <khaledreza@gmail.com>
2024-10-28 13:39:05 +0100

    project initialized with next 15 and react 19 compilier, next-auth implemented for github

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,50 @@
+import { auth, signOut, signIn } from "@/auth";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Navbar = async () => {
+  const session = await auth();
+  return (
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex items-center justify-between">
+        <Link href="/">
+          <Image src="/blog.jpg" alt="" width={80} height={80} />
+        </Link>
+        <div className="flex items-center gap-5 text-black">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut();
+                }}
+              >
+                <button type="submit">Logout</button>
+              </form>
+              <Link href={`/user/${session?.user?.id}}`}>
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+                await signIn();
+              }}
+            >
+              <button type="submit">
+                <span>Login</span>
+              </button>
+            </form>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
